93c75539dd476c5f1b6b89ba27e50a0ea515ce55
Gabriel <fuelanemo149@gmail.com>
2023-09-09 17:30:05 +0200

    Co-authored-by: Terry Gonguet <terry@gonguet.com>

---
+++ src/routes/discussion/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	function connect() {
+		const ws = new WebSocket(`ws://${location.host}/ws?peerId=${id}`)
+		ws.addEventListener("open", () => {
+
+			ws.send("yo")
+		})
+	}
+</script>
